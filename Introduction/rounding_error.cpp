1c7534134ea60d5c5f11eba08d5700d5234dd02e
KristopherTsui <KristopherTsui@163.com>
2020-10-27 10:55:03 +0800

    add exact value and edit comments

--- Introduction/rounding_error.cpp
+++ Introduction/rounding_error.cpp
@@ -10,13 +10,14 @@ double central_difference_quotient(double, double, double (*)(double));
 int main()
 {
 	double x0 = 1;
+	cout << "The exact differential value of f(x) at x0 is " << 3 * x0 * x0 << endl;
 	for(double h = 1; h >= 1e-15; h /= 10)
 	{
 		cout << "When h = " << h << endl;
 		cout.precision(16);
-		cout << "The forward difference quotient of f(x) at x=1 is "
+		cout << "The forward difference quotient of f(x) at x0=1 is "
 			 << forward_difference_quotient(x0, h, f) << endl;
-		cout << "The central difference quotient of f(x) at x=1 is "
+		cout << "The central difference quotient of f(x) at x0=1 is "
 			 << central_difference_quotient(x0, h, f) << endl;
 	}
 
@@ -30,10 +31,10 @@ double f(double x)
 }
 
 /**
- * @brief Calculate the forward difference quotient
- * @param x0 double, 
- * @param h double, the step length of the difference quotient
- * @param (*f)() function pointer, the function to solve
+ * @brief Calculate the forward difference quotient.
+ * @param x0: double, the point at which we calculate the difference quotient
+ * @param h: double, the step length of the difference quotient
+ * @param (*f)() function pointer, the function whose difference quotient we calculate
  *
  * @Returns double, the forward difference quotient of f at x0 with step h
  */
@@ -43,10 +44,10 @@ double forward_difference_quotient(double x0, double h, double (*f)(double x))
 }
 
 /**
- * @brief Calculate the central difference quotient
- * @param x0 double, 
+ * @brief Calculate the central difference quotient.
+ * @param x0 double, the point at which we calculate the difference quotient
  * @param h double, the step length of the difference quotient
- * @param (*f)() function pointer, the function to solve
+ * @param (*f)() function pointer, the function whose difference quotient we calculate
  *
  * @Returns double, the central difference quotient of f at x0 with step h
  */
