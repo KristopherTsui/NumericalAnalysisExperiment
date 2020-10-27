fe4a4d061abb03410aa9d0f784808fe8c5e8f8ab
KristopherTsui <KristopherTsui@163.com>
2020-10-27 16:39:05 +0800

    add maximum iteration number and modify returns

--- NonLinearEquation/newton_iteration.py
+++ NonLinearEquation/newton_iteration.py
@@ -1,58 +1,53 @@
-import math
+from sympy.abc import x
 import sympy as sp
+import math
 
 
-def newton_iteration(x0, func, tol):
-    """ Solve non-linear equation by Newton iteratin method
+def newton_iteration(x0, func, tol=1e-9, Max_iter=100):
+    """ Solve non-linear equation by Newton iteratin method.
 
     Args:
         x0: double, the initial value of the iteration
-        func: Symbol function, the non-linear equation to be solved
-        tol: double, the iteration accurancy
+        func: symbol object, the non-linear equation to be solved
+        tol: double, the iteration accuracy
+        Max_iter: int, maximum iteration number
 
     Returns:
-        iter_list: list, the value of x every iteration
+        k: int, iteration number
+        v: double, root of the non-linear equation
     """
-    x = sp.Symbol('x')
     # derivative
     y_diff = sp.diff(func(x))
-    # begin iteration
+
+    # first iteration
+    k = 1
     u = x0
     v = u - func(u)/y_diff.subs(x, u)
 
-    iter_list = []
-    while math.fabs(v - u) >= tol:
-        iter_list.append(v)
+    # iteration
+    while math.fabs(v - u) >= tol and y_diff.subs(x, v) != 0 and k < Max_iter:
+        k += 1
         u = v
         v = u - func(u)/y_diff.subs(x, u)
 
-    return iter_list
+    return k, v 
 
 
 def f(x):
-    """ The equation to be solved
-
-    Args:
-        x: symbol variable, independent variable
-
-    Returns:
-        y: symbol, the symbol expression of the function
-    """
-    y = sp.Pow(x, 3) + sp.Pow(x, 2) - 3*x - 3
-    return y
+    return sp.Pow(x, 3) + sp.Pow(x, 2) - 3*x - 3
 
 
 if __name__ == '__main__':
     """ The initial values
     When x0 = -2, it converges to another root -sqrt{3}
     When x0 = -1, it is the root of the function
-    When x0 = 0, it doesn't converge
+    When x0 = 0, it converges to -1
     When x0 = 2, it converges to sqrt{3} quickly
     When x0 = 3000000, it converges to sqrt{3} slowly
     """
-    x = [-2.0, -1.0, 0.0, 2.0, 3000000.0]
-    for x0 in x:
-        iter_ls = newton_iteration(x0, f, 1e-6)
-        print(f"The iteration list with the initial value {x0} is:")
-        for item in iter_ls:
-            print(item)
+    x0_lst = [-2.0, -1.0, 0.0, 2.0, 9999999999999999.0]
+    for x0 in x0_lst:
+        n, root = newton_iteration(x0, f, 1e-6)
+        print(f"The root of the non-linear equation with initial value {x0} is {root:.7f}.")
+        print(f"Iteration number is {n}.")
+
