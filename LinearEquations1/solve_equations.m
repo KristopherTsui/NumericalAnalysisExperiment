0497a6fb7a7b2efc2b2d857c295d6db4aa16868b
KristopherTsui <runfengtsui@outlook.com>
2022-09-19 20:58:02 +0800

    move cpp programs to a new directory

--- LinearEquations1/solve_equations.m
+++
@@ -1,9 +0,0 @@
-% input cofficients matrix
-A = [0.101, 2.304, 3.555; -1.347, 3.712, 4.623; -2.835, 1.072, 5.643];
-
-% input constant
-b = [1.183; 2.137; 3.035];
-
-% solve the equations
-x = A\b
-
